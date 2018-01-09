16b5ec3fce5efa0adf45e0bb233298ce769c494a
Domino2015 <812829911@qq.com>
2018-01-09 17:04:59 +0800

    ☕️☕️
    Finished：
    1。获取图1->图2投影矩阵
    2。获得两矩阵之差
    3。归一化显示
    Future:
    1。优化矩阵操作
    2。是否可以完善误差模型

--- main.cpp
+++ main.cpp
@@ -10,17 +10,18 @@ vector<DMatch> ransac(vector<DMatch> matches, vector<KeyPoint> queryKeyPoint, ve
 Point2f getTransformPoint(const Point2f originalPoint,const Mat &transformMaxtri);
 void meshgrid(const cv::Range &xgv, const cv::Range &ygv, cv::Mat &X, cv::Mat &Y);
 void transform(Mat &SrcImage,Mat &dst);
-
+Mat guidedFilter(Mat &srcMat, Mat &guidedMat, int radius, double eps);//引导滤波器
+//将单通道图像转为三通道图像
+Mat convertTo3Channels(const Mat& binImg);
 
 int main() {
 
-
     //读入图片
-    Mat img_1 = imread("../4.jpg");
-    Mat img_2 = imread("../5.jpg");
+    Mat img_1 = imread("../501.png");
+    Mat img_2 = imread("../502.png");
 
     imshow("img_1", img_1);
-    imshow("img_2", img_2);
+    //imshow("img_2", img_2);
 
     //创建SIFT算子
     Ptr<Feature2D> f2d = xfeatures2d::SIFT::create();
@@ -46,13 +47,13 @@ int main() {
     drawMatches(img_1, keypoints_1, img_2, keypoints_2, matches, img_matches);
     //imshow("RANSC_before", img_matches);
     //TODO:这个是是否开启RANSC,具体可以考虑一下？
-/*
+
     //RANSC误匹配点剔除
     matches = ransac(matches, keypoints_1, keypoints_2);
     Mat img_matches1;
     drawMatches(img_1, keypoints_1, img_2, keypoints_2, matches, img_matches1);
     //imshow("RANSC_after", img_matches1);
-*/
+
 
     //获得匹配特征点，并提取最优配对
     sort(matches.begin(),matches.end()); //特征点排序
@@ -64,7 +65,7 @@ int main() {
         imagePoints1.push_back(keypoints_1[matches[i].queryIdx].pt);
         imagePoints2.push_back(keypoints_2[matches[i].trainIdx].pt);
     }
-    cout<<"点个数："<<imagePoints1.size()<<endl;
+    //cout<<"点个数："<<imagePoints1.size()<<endl;
 
 
     //获取图像1到图像2的投影映射矩阵 尺寸为3*3
@@ -126,6 +127,7 @@ int main() {
     //cout<<sub_Homography<<endl;
     //cout<<"经过归一化矩阵："<<endl;
     //cout<<sub_Homography_color<<endl;
+
     //TODO:归一化的点，具体分为两类：sub_Homography_color[i].x 图像1到图像2 ；sub_Homography_color[i].y 图像2到图像1
     for(int i=0;i<imagePoints1.size();i++) {
         circle(img_1, imagePoints1[i], 2, cv::Scalar(sub_Homography_color[i].x,255, 0), 2);
@@ -134,13 +136,60 @@ int main() {
     imshow("[1->2]_sub_Homography",img_1);
     imshow("[2->1]_sub_Homography",img_2);
 
+    Mat M = Mat(img_1.rows, img_1.cols, CV_8UC1,Scalar(17));
+    for(int i=0;i<imagePoints1.size();i++) {
+
+        M.at<uchar>(imagePoints1[i].y+1, imagePoints1[i].x)= sub_Homography_color[i].x;
+        M.at<uchar>(imagePoints1[i].y-1, imagePoints1[i].x)= sub_Homography_color[i].x;
+        M.at<uchar>(imagePoints1[i].y, imagePoints1[i].x+1)= sub_Homography_color[i].x;
+        M.at<uchar>(imagePoints1[i].y, imagePoints1[i].x-1)= sub_Homography_color[i].x;
+        M.at<uchar>(imagePoints1[i].y+1, imagePoints1[i].x+1)= sub_Homography_color[i].x;
+        M.at<uchar>(imagePoints1[i].y+1, imagePoints1[i].x-1)= sub_Homography_color[i].x;
+        M.at<uchar>(imagePoints1[i].y-1, imagePoints1[i].x+1)= sub_Homography_color[i].x;
+        M.at<uchar>(imagePoints1[i].y-1, imagePoints1[i].x-1)= sub_Homography_color[i].x;
+    }
+    //cout<<M<<endl;
+
+    Mat dst_gauss;
+    GaussianBlur(M,dst_gauss,Size(13,13),0,0);
+    Mat dst_gauss_3 = convertTo3Channels(dst_gauss);
+    //imshow("gauss",dst_gauss_3);
+    imwrite("../sub_output.jpg",dst_gauss);
+    cv::Mat im_color;
+    cv::applyColorMap(dst_gauss_3, im_color, cv::COLORMAP_HSV);
+    imshow("color",im_color);
+
+/*
+    Mat dst_guidedFilters;
+    Mat cloneImage = M.clone();
+    dst_guidedFilters = guidedFilter(M,cloneImage,5,0.01);
+    imshow("guidedFilters",dst_guidedFilters);
+*/
+    /*
+    //imshow("finish",M);
+
+    cv::Mat src =imread("../501.png");
+    Mat im_color;
+    cv::applyColorMap(src, im_color, cv::COLORMAP_JET);
+    imshow("color",im_color);
+*/
     //等待任意按键按下
     waitKey(0);
     return 0;
 }
 
-
-
+//将单通道图像转为三通道图像
+Mat convertTo3Channels(const Mat& binImg)
+{
+    Mat three_channel = Mat::zeros(binImg.rows,binImg.cols,CV_8UC3);
+    vector<Mat> channels;
+    for (int i=0;i<3;i++)
+    {
+        channels.push_back(binImg);
+    }
+    merge(channels,three_channel);
+    return three_channel;
+}
 
 
 //RANSAC算法实现
@@ -156,8 +205,8 @@ vector<DMatch> ransac(vector<DMatch> matches, vector<KeyPoint> queryKeyPoint, ve
     Mat homography;
     //保存点对是否保留的标志
     vector<unsigned char> inliersMask(srcPoints.size());
-    //匹配点对进行RANSAC过滤
-    homography = findHomography(srcPoints, dstPoints, CV_RANSAC, 5, inliersMask);
+    //匹配点对进行RANSAC过滤 预值：5
+    homography = findHomography(srcPoints, dstPoints, CV_RANSAC, 30, inliersMask);
     //RANSAC过滤后的点对匹配信息
     vector<DMatch> matches_ransac;
     //手动的保留RANSAC过滤后的匹配点对
@@ -193,4 +242,31 @@ void meshgrid(const cv::Range &xgv, const cv::Range &ygv, cv::Mat &X, cv::Mat &Y
     for (int j = ygv.start; j <= ygv.end; j++) t_y.push_back(float(j));
     cv::repeat(cv::Mat(t_x).t(), t_y.size(), 1, X);
     cv::repeat(cv::Mat(t_y), 1, t_x.size(), Y);
+}
+
+//导向滤波器
+Mat guidedFilter(Mat &srcMat, Mat &guidedMat, int radius, double eps)
+{
+    //------------【0】转换源图像信息，将输入扩展为64位浮点型，以便以后做乘法------------
+    srcMat.convertTo(srcMat, CV_64FC1);
+    guidedMat.convertTo(guidedMat, CV_64FC1);
+    //--------------【1】各种均值计算----------------------------------
+    Mat mean_p, mean_I, mean_Ip, mean_II;
+    boxFilter(srcMat, mean_p, CV_64FC1, Size(radius, radius));//生成待滤波图像均值mean_p
+    boxFilter(guidedMat, mean_I, CV_64FC1, Size(radius, radius));//生成引导图像均值mean_I
+    boxFilter(srcMat.mul(guidedMat), mean_Ip, CV_64FC1, Size(radius, radius));//生成互相关均值mean_Ip
+    boxFilter(guidedMat.mul(guidedMat), mean_II, CV_64FC1, Size(radius, radius));//生成引导图像自相关均值mean_II
+    //--------------【2】计算相关系数，计算Ip的协方差cov和I的方差var------------------
+    Mat cov_Ip = mean_Ip - mean_I.mul(mean_p);
+    Mat var_I = mean_II - mean_I.mul(mean_I);
+    //---------------【3】计算参数系数a、b-------------------
+    Mat a = cov_Ip / (var_I + eps);
+    Mat b = mean_p - a.mul(mean_I);
+    //--------------【4】计算系数a、b的均值-----------------
+    Mat mean_a, mean_b;
+    boxFilter(a, mean_a, CV_64FC1, Size(radius, radius));
+    boxFilter(b, mean_b, CV_64FC1, Size(radius, radius));
+    //---------------【5】生成输出矩阵------------------
+    Mat dstImage = mean_a.mul(srcMat) + mean_b;
+    return dstImage;
 }
